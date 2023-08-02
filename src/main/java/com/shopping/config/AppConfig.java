626e77cba1fb9dc7b085844bf029e997e4d27d5f
Vikash Katiyar <rishabktr018@gmail.com>
2023-08-02 15:15:51 +0530

    update cors

--- src/main/java/com/shopping/config/AppConfig.java
+++ src/main/java/com/shopping/config/AppConfig.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -43,7 +42,7 @@ public class AppConfig {
 								"http://localhost:3000", 
 								"http://localhost:4000",
 								"http://localhost:4200",
-								"https://shopwithzosh.vercel.app"
+								"https://clothify-shopping.vercel.app/"
 								
 							)
 						);
